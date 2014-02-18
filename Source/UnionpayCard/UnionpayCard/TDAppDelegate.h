9f0fbeaa527744cb8f2cfa9db5411602c4eff240
Daniel Dong <ydong@gagein.com>
2014-02-18 18:41:07 +0800

    create project & add images

---
+++ Source/UnionpayCard/UnionpayCard/TDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDAppDelegate.h
+//  UnionpayCard
+//
+//  Created by Dong Yiming on 2/18/14.
+//  Copyright (c) 2014 Frodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
