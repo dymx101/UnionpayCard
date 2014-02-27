cbf760c4d62b6f67ae3e434dfb1cb70189e73d05
Daniel Dong <ydong@gagein.com>
2014-02-27 17:43:24 +0800

    working on TDCardListVC

--- Source/UnionpayCard/UnionpayCard/Src/ViewControllers/SubVCs/TDCardListVC.m
+++ Source/UnionpayCard/UnionpayCard/Src/ViewControllers/SubVCs/TDCardListVC.m
@@ -7,8 +7,12 @@
 //
 
 #import "TDCardListVC.h"
+#import "UIView+Effect.h"
 
-@interface TDCardListVC ()
+@interface TDCardListVC () {
+    UIView *_topBarShadowView;
+    UIView *_topBarView;
+}
 
 @end
 
@@ -17,7 +21,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //self.view.backgroundColor = [FDColor sharedInstance].red;
     self.navigationItem.title = @"卡片管理";
+    
+    [self createViews];
+    [self layoutViews];
+}
+
+-(void)createViews {
+    _topBarShadowView = [UIView new];
+    _topBarShadowView.backgroundColor = [FDColor sharedInstance].black;
+    [_topBarShadowView applyEffectShadow];
+    [self.view addSubview:_topBarShadowView];
+    
+    _topBarView = [UIView new];
+    _topBarView.backgroundColor = [FDColor sharedInstance].white;
+    [self.view addSubview:_topBarView];
+}
+
+-(void)layoutViews {
+    [_topBarShadowView constrainWidthToView:self.view predicate:nil];
+    [_topBarShadowView constrainHeight:@"40"];
+    [_topBarShadowView alignTopEdgeWithView:self.view predicate:nil];
+    [_topBarShadowView alignCenterXWithView:self.view predicate:nil];
+    
+    [_topBarView alignToView:_topBarShadowView];
 }
 
 @end
