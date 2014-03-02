01c49a3f07012472097d0c8c8d0b42b2a265c1f1
iamtowne <176488411@qq.com>
2014-03-02 19:56:42 +0800

    update url
    update url

--- Source/UnionpayCard/UnionpayCard/Src/APIEngine/TDHttpClient.m
+++ Source/UnionpayCard/UnionpayCard/Src/APIEngine/TDHttpClient.m
@@ -10,9 +10,9 @@
 #import "TDJsonParser.h"
 
 //http://localhost:8000/yscardII/json/Show/{"method":"showBtype"}
-//static NSString *const BASEURL = @"http://localhost:8000/yscardII/json/";
+static NSString *const BASEURL = @"http://localhost:8000/yscardII/json/";
 
-static NSString *const BASEURL = @"http://113.57.133.84:8081/yscardII/json/"; // 外网地址
+//static NSString *const BASEURL = @"http://113.57.133.84:8081/yscardII/json/"; // 外网地址
 
 @implementation TDHttpClient
 
