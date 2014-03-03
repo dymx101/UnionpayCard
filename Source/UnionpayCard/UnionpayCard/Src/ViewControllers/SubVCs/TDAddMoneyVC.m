01fb2339930c8362f2f329710a9138ce5b67d5b7
Daniel Dong <ydong@gagein.com>
2014-03-03 22:01:36 +0800

    充值&消费 记录

--- Source/UnionpayCard/UnionpayCard/Src/ViewControllers/SubVCs/TDAddMoneyVC.m
+++ Source/UnionpayCard/UnionpayCard/Src/ViewControllers/SubVCs/TDAddMoneyVC.m
@@ -8,9 +8,14 @@
 
 #import "TDAddMoneyVC.h"
 #import "TDRecordCell.h"
+#import "UIView+Effect.h"
 
 @interface TDAddMoneyVC () <UITableViewDelegate, UITableViewDataSource> {
     UITableView     *_tv;
+    UIButton        *_btnAddMoneyRec;
+    UIButton        *_btnUseMoneyRec;
+    
+    BOOL            _showAddMoneyRecord;
 }
 
 @end
@@ -22,8 +27,12 @@
     [super viewDidLoad];
     self.navigationItem.title = @"消费充值记录";
     
+    _showAddMoneyRecord = YES;
+    
     [self createViews];
     [self layoutViews];
+    
+    [_tv reloadData];
 }
 
 -(void)createViews {
@@ -51,11 +60,92 @@
         cell = [TDRecordCell new];
     }
     
+    cell.btnFreeze.hidden = !_showAddMoneyRecord;
+    if (_showAddMoneyRecord) {
+        cell.lblVendorName.text = @"仟吉西饼";
+        cell.lblCardNumber.text = @"卡号: 12378378123871283";
+        cell.lblAmount.text = @"充值金额: ￥200.00";
+        cell.lblFlowNumber.text = @"充值流水号: 34573784537456347";
+    } else {
+        cell.lblVendorName.text = @"周黑鸭";
+        cell.lblCardNumber.text = @"卡号: 2348348385345835";
+        cell.lblAmount.text = @"消费金额: ￥100.00";
+        cell.lblFlowNumber.text = @"消费流水号: 34573784537456347";
+    }
+    
     return cell;
 }
 
 -(float)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return [TDRecordCell HEIGHT];
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    static UIView *header = nil;
+    if (header == nil) {
+        header = [UIView new];
+        header.backgroundColor = [UIColor blackColor];
+        [header applyEffectShadow];
+        
+        UIView *bgView = [UIView new];
+        bgView.backgroundColor = [FDColor sharedInstance].silver;
+        [header addSubview:bgView];
+        [bgView alignToView:header];
+        
+        //
+        _btnAddMoneyRec = [UIButton new];
+        [_btnAddMoneyRec setTitle:@"充值记录" forState:UIControlStateNormal];
+        [_btnAddMoneyRec setTitleColor:[FDColor sharedInstance].themeBlue forState:UIControlStateHighlighted];
+        [_btnAddMoneyRec setTitleColor:[FDColor sharedInstance].gray forState:UIControlStateNormal];
+        [_btnAddMoneyRec setTitleColor:[FDColor sharedInstance].black forState:UIControlStateSelected];
+        _btnAddMoneyRec.titleLabel.font = [TDFontLibrary sharedInstance].fontTitleBold;
+        
+        [_btnAddMoneyRec addTarget:self action:@selector(addMoneyRecordAction:) forControlEvents:UIControlEventTouchUpInside];
+        [bgView addSubview:_btnAddMoneyRec];
+        [_btnAddMoneyRec alignLeadingEdgeWithView:bgView predicate:@"0"];
+        [_btnAddMoneyRec constrainHeightToView:bgView predicate:nil];
+        [_btnAddMoneyRec alignCenterYWithView:bgView predicate:nil];
+        [_btnAddMoneyRec constrainWidthToView:bgView predicate:@"*.5"];
+        _btnAddMoneyRec.selected = _showAddMoneyRecord;
+        
+        //
+        _btnUseMoneyRec = [UIButton new];
+        [_btnUseMoneyRec setTitle:@"消费记录" forState:UIControlStateNormal];
+        [_btnUseMoneyRec setTitleColor:[FDColor sharedInstance].themeBlue forState:UIControlStateHighlighted];
+        _btnUseMoneyRec.titleLabel.font = [TDFontLibrary sharedInstance].fontTitleBold;
+        
+        [_btnUseMoneyRec addTarget:self action:@selector(useMoneyRecordAction:) forControlEvents:UIControlEventTouchUpInside];
+        [_btnUseMoneyRec setTitleColor:[FDColor sharedInstance].gray forState:UIControlStateNormal];
+        [_btnUseMoneyRec setTitleColor:[FDColor sharedInstance].black forState:UIControlStateSelected];
+        [bgView addSubview:_btnUseMoneyRec];
+        [_btnUseMoneyRec alignTrailingEdgeWithView:bgView predicate:@"0"];
+        [_btnUseMoneyRec constrainHeightToView:bgView predicate:nil];
+        [_btnUseMoneyRec alignCenterYWithView:bgView predicate:nil];
+        [_btnUseMoneyRec constrainWidthToView:bgView predicate:@"*.5"];
+    }
+    
+    return header;
+}
+
+-(float)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 50;
+}
+
+#pragma mark - 
+-(void)addMoneyRecordAction:(id)sender {
+    _showAddMoneyRecord = YES;
+    _btnAddMoneyRec.selected = _showAddMoneyRecord;
+    _btnUseMoneyRec.selected = !_showAddMoneyRecord;
+    
+    [_tv reloadData];
+}
+
+-(void)useMoneyRecordAction:(id)sender {
+    _showAddMoneyRecord = NO;
+    _btnAddMoneyRec.selected = _showAddMoneyRecord;
+    _btnUseMoneyRec.selected = !_showAddMoneyRecord;
+    
+    [_tv reloadData];
 }
 
 @end
--- Source/UnionpayCard/UnionpayCard/Src/Views/TableCells/TDRecordCell.m
+++ Source/UnionpayCard/UnionpayCard/Src/Views/TableCells/TDRecordCell.m
@@ -25,6 +25,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
         [self createViews];
     }
     return self;
@@ -39,10 +40,56 @@
     
     [self addSubview:_bgView];
     [_bgView alignTop:@"2" leading:@"5" bottom:@"-2" trailing:@"-5" toView:self];
+    
+    //
+    _lblVendorName = [UILabel new];
+    _lblVendorName.text = @"仟吉西饼";
+    _lblVendorName.font = [TDFontLibrary sharedInstance].fontTitleBold;
+    _lblVendorName.textColor = [FDColor sharedInstance].themeBlue;
+    [self addSubview:_lblVendorName];
+    [_lblVendorName alignTop:@"10" leading:@"20" toView:self];
+    
+    //
+    _lblCardNumber = [UILabel new];
+    _lblCardNumber.text = @"卡号:23467384628374";
+    _lblCardNumber.font = [TDFontLibrary sharedInstance].fontNormal;
+    _lblCardNumber.textColor = [FDColor sharedInstance].gray;
+    [self addSubview:_lblCardNumber];
+    [_lblCardNumber alignCenterYWithView:_lblVendorName predicate:nil];
+    [_lblCardNumber constrainLeadingSpaceToView:_lblVendorName predicate:@"10"];
+    
+    //
+    _lblAmount = [UILabel new];
+    _lblAmount.text = @"充值金额：￥200.00";
+    _lblAmount.font = [TDFontLibrary sharedInstance].fontNormal;
+    _lblAmount.textColor = [FDColor sharedInstance].red;
+    [self addSubview:_lblAmount];
+    [_lblAmount constrainTopSpaceToView:_lblVendorName predicate:@"10"];
+    [_lblAmount alignLeadingEdgeWithView:_lblVendorName predicate:@"0"];
+    
+    //
+    _lblFlowNumber = [UILabel new];
+    _lblFlowNumber.text = @"充值流水号: 1376746182361837126378162";
+    _lblFlowNumber.font = [TDFontLibrary sharedInstance].fontNormal;
+    _lblFlowNumber.textColor = [FDColor sharedInstance].lightGray;
+    [self addSubview:_lblFlowNumber];
+    [_lblFlowNumber constrainTopSpaceToView:_lblAmount predicate:@"10"];
+    [_lblFlowNumber alignLeadingEdgeWithView:_lblVendorName predicate:@"0"];
+    
+    //
+    _btnFreeze = [UIButton new];
+    [_btnFreeze setBackgroundImage:[TDImageLibrary sharedInstance].btnBgGreen forState:UIControlStateNormal];
+    [_btnFreeze setTitle:@"冻结" forState:UIControlStateNormal];
+    _btnFreeze.titleLabel.font = [TDFontLibrary sharedInstance].fontNormal;
+    [self addSubview:_btnFreeze];
+    [_btnFreeze alignCenterYWithView:_lblFlowNumber predicate:nil];
+    [_btnFreeze alignTrailingEdgeWithView:_bgView predicate:@"-10"];
+    [_btnFreeze constrainWidth:@"70" height:@"30"];
 }
 
 +(float)HEIGHT {
     return MY_HEIGHT;
 }
+
 
 @end
